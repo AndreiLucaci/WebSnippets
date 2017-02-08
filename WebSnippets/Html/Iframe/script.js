8098dc7073fd8f6931ccfad72e4313514ee4c089
Lucaci Andrei <andreilucaci@windowslive.com>
2017-02-09 00:30:16 +0200

    Added iframe code

---
+++ WebSnippets/Html/Iframe/script.js
@@ -0,0 +1,10 @@
+﻿(function ($, document) {
+	$(document).ready(function () {
+		$("#clicker").on("click", function () {
+			var texterValue = $("#texter").val();
+			var iframeWindow = document.defaultView || document.parentWindow;;
+			$(iframeWindow.parent.document).find(".left .hrefs a").first().text(texterValue);
+		});
+	});
+}
+)(jQuery, window.document);
